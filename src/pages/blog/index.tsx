f4db8d20ab131a6868ca5343ca49f40ac4de6853
JX_PPP <jhl2554937602@gmail.com>
2024-02-27 16:17:35 +0800

    🐛fix: 临时提交

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,34 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+
+import Vditor from "vditor";
+import "vditor/dist/index.css";
 
 import BlogItem from "@/components/blogItem";
 import { BlogContainer, BlogHeader } from "./styles";
 
+/* 
+  blog相关功能：
+    1. 预览，从服务端获取
+    2. 在线编辑，支持保存、发布等功能
+*/
+
 const BlogPage: FC = () => {
+  // const [vd, setVd] = useState<Vditor>()
+
+  useEffect(() => {
+    const vditor = new Vditor("vditor", {
+      height: 300,
+      width: "100%",
+      // after() {
+      //   setVd(vditor)
+      // }
+    });
+    return () => {
+      vditor.destroy();
+      // setVd(undefined);
+    };
+  }, []);
+
   return (
     <BlogContainer>
       <BlogHeader>Blog</BlogHeader>
@@ -12,6 +37,8 @@ const BlogPage: FC = () => {
       <BlogItem />
       <BlogItem />
       <BlogItem />
+
+      <div id="vditor" className="vditor" />
     </BlogContainer>
   );
 };
